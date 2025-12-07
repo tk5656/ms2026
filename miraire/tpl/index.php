722b5f71730bfe6628ddf600cdd765e03b25123f
kazuki <kazuki22331@gmail.com>
2025-12-08 08:38:14 +0900

    更新

--- miraire/tpl/index.php
+++ miraire/tpl/index.php
@@ -8,6 +8,8 @@
     <link rel="stylesheet" href="css/destyle.css">
     <!-- Google fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&family=Noto+Sans+JP:wght@100..900&family=Noto+Serif+JP:wght@200..900&display=swap" rel="stylesheet">
+    <!-- Adobe fonts -->
+     <script src="./js/Adobe_fonts.js"></script>
     <!-- CSS -->
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/page/index/index.css">
@@ -16,17 +18,17 @@
 </head>
 <body>
     <header>
-        <img src="./images/header_logo.svg" alt="ミライレのロゴ">
+        <img class="logo" src="./images/header_logo.svg" alt="ミライレのロゴ">
         <nav>
             <ul>
-                <li>About</li>
-                <li>News</li>
-                <li>Chera</li>
+                <li>About<span>本展示について</span></li>
+                <li>News<span>最新情報</span></li>
+                <li>Chera<span>公約・キャラクター</span></li>
             </ul>
         </nav>
+        <img class="vote" src="./images/header_box.svg" alt="投票箱">
     </header>
     <!-- JS -->
-     <script src="main.js"></script>
     <!-- <script src="js/script.js"></script> -->
 </body>
 </html>
