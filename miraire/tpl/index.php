0c2291dfb4eb3e46f1108c2ae08ec7a00b62a982
kazuki <kazuki22331@gmail.com>
2025-12-09 09:57:59 +0900

    更新

--- miraire/tpl/index.php
+++ miraire/tpl/index.php
@@ -18,7 +18,9 @@
 </head>
 <body>
     <header>
-        <img class="logo" src="./images/header_logo.svg" alt="ミライレのロゴ">
+        <h1>
+            <img class="logo" src="./images/header_logo.svg" alt="ミライレのロゴ">
+        </h1>
         <nav>
             <ul>
                 <li>About<span>本展示について</span></li>
@@ -30,8 +32,49 @@
     </header>
     <main>
         <section class="top">
+            <img src="./images/top_samune.png" alt="">
+        </section>
+        <section class="about">
+            <div class="text-area">
+                <h2>About</h2>
+                <p class="catchcopy">投票体験そのものを<br>今よりもっと楽しく。</p>
+                <p class="text">
+                    各公約ごとに個性溢れるキャラクターたちが登場し、あなたの投票体験をより楽しいものにします。<br>
+                    また、投票ごとに選んだ公約の建物の様子が変化し、一票の大切さをより実感できる展示となっています。
+                </p>
+                <p class="link"><a href="">公約・キャラクターを見る</a></p>
+            </div>
+        </section>
+        <section class="explanation">
+            <div class="contents">
+                <div class="item">
+                    <img src="" alt="">
+                    <h3>魅力的な動物たち</h3>
+                    <p>ここにはアピールポイント<br>の説明文などが<br>入ると思います。</p>
+                </div>
+                <div class="item">
+                    <img src="" alt="">
+                    <h3>未来の姿を可視化</h3>
+                    <p>ここにはアピールポイント<br>の説明文などが<br>入ると思います。</p>
+                </div>
+                <div class="item">
+                    <img src="" alt="">
+                    <h3>あと何か</h3>
+                    <p>ここにはアピールポイント<br>の説明文などが<br>入ると思います。</p>
+                </div>
+            </div>
+            <p class="link"><a href="">現在の投票状況</a></p>
         </section>
     </main>
+    <footer>
+        <img class="logo" src="../images/hooter_logo.png" alt="">
+        <ul>
+            <li><a href="">本展示について</a></li>
+            <li><a href="">公約・キャラクター</a></li>
+            <li><a href="">最新情報</a></li>
+        </ul>
+        <img class="sns" src="../images/hooter_SNS.png" alt="">
+    </footer>
     <!-- JS -->
     <!-- <script src="js/script.js"></script> -->
 </body>
