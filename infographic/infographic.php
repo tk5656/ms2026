aab8b10f0d5e1c3de6dc61340859a1fd82ca7839
kazuki <kazuki22331@gmail.com>
2025-12-11 17:23:46 +0900

    infographicページ作成

---
+++ infographic/infographic.php
@@ -0,0 +1,3 @@
+<?php
+require_once "./tpl/infographic.php";
+?>
---
+++ infographic/tpl/infographic.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>infographic</title>
+    <!-- cssファイル読み込み -->
+    <link rel="stylesheet" href="./css/destyle.css">
+    <link rel="stylesheet" href="./css/infographic.css">
+</head>
+<body>
+    <h1>現在の投票状況</h1>
+    <div class="ranking">
+        <div class="ranking-item">
+            <p class="number">1</p>
+            <div class="pledge-details">
+                <p class="pledge">ビヤガーデン公約仮仮仮仮仮仮</p>
+                <div>
+                    <p class="name">苗字コケゾウ</p>
+                    <p class="age">35歳</p>
+                </div>
+            </div>
+            <div class="gauge">
+                <div class="now-gauge"></div>
+            </div>
+        </div>
+        <div class="winning-border">
+            <p>当選</p>
+            <div></div>
+        </div>
+        <div class="ranking-item">
+            <p class="number">2</p>
+            <div class="pledge-details">
+                <p class="pledge">ビヤガーデン公約仮仮仮仮仮仮</p>
+                <div>
+                    <p class="name">苗字コケゾウ</p>
+                    <p class="age">35歳</p>
+                </div>
+            </div>
+            <div class="gauge">
+                <div class="now-gauge"></div>
+            </div>
+        </div>
+        <div class="ranking-item">
+            <p class="number">3</p>
+            <div class="pledge-details">
+                <p class="pledge">ビヤガーデン公約仮仮仮仮仮仮</p>
+                <div>
+                    <p class="name">苗字コケゾウ</p>
+                    <p class="age">35歳</p>
+                </div>
+            </div>
+            <div class="gauge">
+                <div class="now-gauge"></div>
+            </div>
+        </div>
+        <div class="ranking-item">
+            <p class="number">4</p>
+            <div class="pledge-details">
+                <p class="pledge">ビヤガーデン公約仮仮仮仮仮仮</p>
+                <div>
+                    <p class="name">苗字コケゾウ</p>
+                    <p class="age">35歳</p>
+                </div>
+            </div>
+            <div class="gauge">
+                <div class="now-gauge"></div>
+            </div>
+        </div>
+        <div class="ranking-item">
+            <p class="number">5</p>
+            <div class="pledge-details">
+                <p class="pledge">ビヤガーデン公約仮仮仮仮仮仮</p>
+                <div>
+                    <p class="name">苗字コケゾウ</p>
+                    <p class="age">35歳</p>
+                </div>
+            </div>
+            <div class="gauge">
+                <div class="now-gauge"></div>
+            </div>
+        </div>
+    </div>
+    <div class="sub-content">
+        <div class="animation">
+        </div>
+        <p class="voting-status">投票<br>状況</p>
+        <div class="vote-count">
+            <div class="content">
+                <p class="count-num">00</p>
+            </div>
+            <div class="content">
+                <p class="count-num">00</p>
+            </div>
+            <div class="content">
+                <p class="count-num">00</p>
+            </div>
+            <div class="content">
+                <p class="count-num">00</p>
+            </div>
+            <div class="content">
+                <p class="count-num">00</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
