626c39fa685669b8c3f78b44e6db073e2237cd15
kazuki <kazuki22331@gmail.com>
2025-12-14 11:24:37 +0900

    各ページ修正

---
+++ miraire/tpl/voting_completed.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>投票完了</title>
+    <!-- destyle CSS -->
+    <link rel="stylesheet" href="css/destyle.css">
+    <!-- Adobe fonts -->
+     <script src="./js/Adobe_fonts.js"></script>
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/page/index/index.css">
+</head>
+<body>
+    <header>
+        <h1>
+            <img class="logo" src="./images/header_logo.svg" alt="ミライレのロゴ">
+        </h1>
+        <nav>
+            <ul>
+                <li><a href="#about">About<span>本展示について</span></a></li>
+                <li><a href="">News<span>最新情報</span></a></li>
+                <li><a href="./character.html">Chara<span>公約・キャラクター</span></a></li>
+            </ul>
+        </nav>
+        <p class="vote">
+            <a href=""><img class="vote-img" src="./images/header_box.png" alt="投票箱"></a>
+        </p>
+    </header>
+    <main>
+        
+    </main>
+    <footer>
+        <div class="footer-logo">
+            <img class="logo" src="./images/hooter_logo.png" alt="">
+        </div>
+        <ul>
+            <li><a href="">本展示について</a></li>
+            <li><a href="">公約・キャラクター</a></li>
+            <li><a href="">最新情報</a></li>
+        </ul>
+        <div class="sns-link">
+            <img class="sns" src="./images/hooter_SNS.png" alt="">
+        </div>
+    </footer>
+</body>
+</html>
---
+++ miraire/voting_completed.php
@@ -0,0 +1,2 @@
+<?php
+require_once "./tpl/voting_completed.php";
